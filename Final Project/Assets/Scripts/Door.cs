ac67a03e3327cf2ca943991cc02af0dbe96539e9
Khaledm12 <155543842+Khaledm12@users.noreply.github.com>
2026-09-17 00:34:46 +0400

    door sound hotfix

--- Final Project/Assets/Scripts/Door.cs
+++ Final Project/Assets/Scripts/Door.cs
@@ -4,13 +4,14 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private AudioSource openSound;
+    [SerializeField] private float soundDelay = 0.5f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             anim.SetBool("Near", true);
-            if (openSound != null) openSound.Play();
+            if (openSound != null) openSound.PlayDelayed(soundDelay);
         }
     }
 
